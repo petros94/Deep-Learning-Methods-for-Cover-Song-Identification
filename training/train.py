d7654d2d0337b587ddf1a583df1729bab20bfec0
petros <petros_94@icloud.com>
2022-09-15 23:39:31 +0300

    minor

--- training/train.py
+++ training/train.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 from training.train_cnn_triplet import train_triplet_loss
 
-def train(model, train_set, valid_set, config_path, dir_name):
+def train(model, train_set, valid_set, config_path, checkpoint_dir):
     with open(config_path, "r") as f:
         config = json.load(f)
                 
     if config['loss'] == 'triplet' and config['model']['type'] in ('resnet18'):
-        return train_triplet_loss(model, train_set, valid_set, config['train']['n_epochs'], config['train']['batch_size'], config['train']['lr'], dir_name)
+        return train_triplet_loss(model, train_set, valid_set, config['train']['n_epochs'], config['train']['batch_size'], config['train']['lr'], checkpoint_dir)
