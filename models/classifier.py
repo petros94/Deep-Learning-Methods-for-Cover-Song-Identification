e12c63b8711a0b444b1b2ef3bce7b552951e863a
petros <petros_94@icloud.com>
2022-09-18 12:27:39 +0300

    fix

--- models/classifier.py
+++ models/classifier.py
@@ -3,7 +3,7 @@ import torch
 
 class ThresholdClassifier(nn.Module):
     def __init__(self, model, D) -> None:
-        super().__init__(self)
+        super(ThresholdClassifier, self).__init__()
         self.D = D
         self.model = model
         
