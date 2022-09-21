8a93e14c3c7df340dd91e944981fe2a134631b0e
petros <petros_94@icloud.com>
2022-09-22 01:03:00 +0300

    fix cnn output

--- models/classifier.py
+++ models/classifier.py
@@ -11,4 +11,5 @@ class ThresholdClassifier(nn.Module):
         out1 = self.model(x1)
         out2 = self.model(x2)
         dist = torch.norm(out1 - out2, dim=1)
-        return (dist < self.D)*1
+        inv = 1/dist
+        return (inv > self.D)*1
