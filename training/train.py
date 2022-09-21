5b262c407b803e665e6cbe96ac69127d0ada6c7c
petros <petros_94@icloud.com>
2022-09-21 11:03:55 +0300

    fix

--- training/train.py
+++ training/train.py
@@ -7,7 +7,7 @@ def train(model, train_set, valid_set, config_path, checkpoint_dir, results_dir)
     with open(config_path, "r") as f:
         config = json.load(f)
                 
-    if config['loss'] == 'triplet' and config['model']['type'] in ('resnet18'):
+    if config['loss'] == 'triplet':
         return train_triplet_loss(model, train_set, valid_set, 
                                   config['train']['n_epochs'], config['train']['patience'], config['train']['batch_size'], config['train']['lr'], 
                                   checkpoint_dir, results_dir)
