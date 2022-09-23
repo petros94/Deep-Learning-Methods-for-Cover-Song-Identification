8369c1fb110d55a3bf4711d14b4858bf77de5558
petros <petros_94@icloud.com>
2022-09-23 19:02:58 +0300

    clf output

--- models/classifier.py
+++ models/classifier.py
@@ -12,4 +12,4 @@ class ThresholdClassifier(nn.Module):
         out2 = self.model(x2)
         dist = torch.norm(out1 - out2, dim=1)
         inv = 1/dist
-        return (inv > self.D)*1
+        return (inv > self.D)*1, dist
