276c8f3666c1cae20735df78370ea12fe7b2b698
petros <petros_94@icloud.com>
2022-09-26 22:46:34 +0300

    bug fix

--- training/train_cnn_hard_triplet.py
+++ training/train_cnn_hard_triplet.py
@@ -17,10 +17,10 @@ def RandomMiner(embeddings, labels):
         pos_ids = torch.argwhere(labels == label).squeeze()
         pos = idx
         while pos == idx:
-            pos = random.choice(pos_ids)
+            pos = random.choice(pos_ids).item()
             
         neg_ids = torch.argwhere(labels != label).squeeze()
-        neg = random.choice(neg_ids)
+        neg = random.choice(neg_ids).item()
         
         anchors.append(anchor)
         positives.append(pos)
