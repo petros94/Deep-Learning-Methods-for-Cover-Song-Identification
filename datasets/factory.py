aeb9db5efc8ece926bb2ab1e54d13f0fcd565d19
petros <petros_94@icloud.com>
2022-10-11 23:20:26 +0300

    minor

--- datasets/factory.py
+++ datasets/factory.py
@@ -13,7 +13,8 @@ def make_dataset(songs: dict, config_path: str, type: str):
     
     if type == 'triplet':
         return TripletDataset(songs, 
-                              samples_per_song=config['features']['samples_per_song'], 
+                              n_batches=config['features']['n_batches'], 
+                              songs_per_batch=config['features']['songs_per_batch'],
                               frame_size=config['features']['frame_size'],
-                              scale=config['features']['scale'])       
+                              scale=config['features']['scale'])      
         
