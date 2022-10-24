bbb04afd25b42e83492320c6aa4ae97000910b76
petros <petros_94@icloud.com>
2022-10-24 23:56:45 +0300

    added lstm

--- models/factory.py
+++ models/factory.py
@@ -1,13 +1,15 @@
 import json
 from models.resnet18.ResNet18 import from_config as make_resnet
 from models.cnn.cnn import from_config as make_cnn
+from models.lstm.lstm import from_config as make_lstm
 import torch
 from utils.generic import get_device
 
 
-AVAILABLE_TYPES = ["resnet18", "cnn"]
+AVAILABLE_TYPES = ["resnet18", "cnn", "lstm"]
 
-def make_model(config_path: str = 'models/config.json'):
+
+def make_model(config_path: str = "models/config.json"):
     """Factory to generate models from configuration
 
     Args:
@@ -22,23 +24,27 @@ def make_model(config_path: str = 'models/config.json'):
     """
     with open(config_path, "r") as f:
         config = json.load(f)
-        
-    if config['model']['type'] not in AVAILABLE_TYPES:
+
+    if config["model"]["type"] not in AVAILABLE_TYPES:
         raise ValueError("Invalid type")
-    
-    if config['model']['type'] == "resnet18":
-        model = make_resnet(config_path=config['model']['config_path'])
-    elif config['model']['type'] == "cnn":
-        model = make_cnn(config_path=config['model']['config_path'])
-        
-    if config['model']['checkpoint_path'] is not None:
+
+    if config["model"]["type"] == "resnet18":
+        model = make_resnet(config_path=config["model"]["config_path"])
+    elif config["model"]["type"] == "cnn":
+        model = make_cnn(config_path=config["model"]["config_path"])
+    elif config["model"]["type"] == "lstm":
+        model = make_lstm(config_path=config["model"]["config_path"])
+
+    if config["model"]["checkpoint_path"] is not None:
         loc = get_device()
-        chk = torch.load(config['model']['checkpoint_path'], map_location=torch.device(loc)) 
+        chk = torch.load(
+            config["model"]["checkpoint_path"], map_location=torch.device(loc)
+        )
         print("loaded pretrained model")
-        
-        model.load_state_dict(chk['model_state_dict'])
-        epoch = chk['epoch']
-        loss = chk['loss']
+
+        model.load_state_dict(chk["model_state_dict"])
+        epoch = chk["epoch"]
+        loss = chk["loss"]
         model.eval()
-        
+
     return model
---
+++ models/lstm/lstm.py
@@ -0,0 +1,34 @@
+from torch import nn
+import torch
+import json
+
+class LSTM(nn.Module):
+  def __init__(self, input_size, hidden_size, num_layers, output_size, bidirectional=False) -> None:
+      super(LSTM, self).__init__()
+      self.input_size = input_size
+      self.num_layers = num_layers
+      self.hidden_size = hidden_size
+      self.bidirectional = bidirectional
+      self.lstm = nn.LSTM(input_size = self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers, bidirectional=bidirectional, dropout=0.2)
+      self.output_size = output_size
+      self.fc = nn.Linear(in_features=(2 if bidirectional else 1)*self.hidden_size, out_features=output_size)
+      self.dropout = nn.Dropout(0.2)
+      # self.fc1 = nn.Linear(in_features=(2 if bidirectional else 1)*self.hidden_size, out_features=128)
+      # self.fc2 = nn.Linear(128, out_features=output_size)
+
+  def forward(self, x):
+    out_packed, (h, c) = self.lstm(x)
+    cat = torch.cat((h[-1, :, :], h[-2, :, :]), dim=1) if self.bidirectional else h[-1, :, :]
+    x = self.fc(cat)
+    return x.view(-1, self.output_size)
+
+
+def from_config(config_path: str):
+    """Create a cnn model from configuration
+
+    Args:
+        config_path (str): The path to the configuration file
+    """
+    with open(config_path, "r") as f:
+        config = json.load(f)
+        return LSTM(**config)
