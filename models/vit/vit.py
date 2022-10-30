a22bfc1ef6b289755fa0483b64ab5a0d86e44896
petros <petros_94@icloud.com>
2022-10-30 16:27:25 +0200

    model size

--- models/vit/vit.py
+++ models/vit/vit.py
@@ -6,10 +6,16 @@ from torch.nn import functional as F
 
 
 class ViT(nn.Module):
-    def __init__(self, patch_size=4, image_size=128) -> None:
+    def __init__(
+        self, patch_size=4, image_size=128, num_hidden_layers=12, num_attention_heads=12
+    ) -> None:
         super().__init__()
         self.configuration = ViTConfig(
-            num_channels=1, patch_size=patch_size, image_size=image_size
+            num_channels=1,
+            patch_size=patch_size,
+            image_size=image_size,
+            num_hidden_layers=num_hidden_layers,
+            num_attention_heads=num_attention_heads,
         )
         self.model = ViTModel(self.configuration)
         self.image_size = image_size
@@ -30,4 +36,9 @@ def from_config(config_path: str):
     """
     with open(config_path, "r") as f:
         config = json.load(f)
-        return ViT(patch_size=config["patch_size"], image_size=config["image_size"])
+        return ViT(
+            patch_size=config["patch_size"],
+            image_size=config["image_size"],
+            num_hidden_layers=config["num_hidden_layers"],
+            num_attention_heads=config["num_attention_heads"],
+        )
