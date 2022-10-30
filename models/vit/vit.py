c0bc4fa1cab1b423c5f8e41c89c24401f49886ae
petros <petros_94@icloud.com>
2022-10-30 16:03:47 +0200

    changes

--- models/vit/vit.py
+++ models/vit/vit.py
@@ -8,7 +8,9 @@ from torch.nn import functional as F
 class ViT(nn.Module):
     def __init__(self, patch_size=4, image_size=128) -> None:
         super().__init__()
-        self.configuration = ViTConfig(num_channels=1, patch_size=patch_size)
+        self.configuration = ViTConfig(
+            num_channels=1, patch_size=patch_size, image_size=image_size
+        )
         self.model = ViTModel(self.configuration)
         self.image_size = image_size
 
