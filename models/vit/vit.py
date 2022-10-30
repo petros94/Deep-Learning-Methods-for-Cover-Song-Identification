37848f892f7f69f2acbc1d9815b73349f6d236df
petros <petros_94@icloud.com>
2022-10-30 16:02:09 +0200

    fixes

--- models/vit/vit.py
+++ models/vit/vit.py
@@ -2,18 +2,22 @@ from transformers import ViTFeatureExtractor, ViTModel, ViTConfig
 import torch
 from torch import nn
 import json
+from torch.nn import functional as F
+
 
 class ViT(nn.Module):
-    def __init__(self, patch_size=4) -> None:
+    def __init__(self, patch_size=4, image_size=128) -> None:
         super().__init__()
         self.configuration = ViTConfig(num_channels=1, patch_size=patch_size)
         self.model = ViTModel(self.configuration)
-        self.feature_extractor = ViTFeatureExtractor(do_resize=False, do_normalize=False)
-        
+        self.image_size = image_size
+
     def forward(self, x):
-        inputs = self.feature_extractor(x, return_tensors="pt")
+        x = F.interpolate(x, size=self.image_size)
+        inputs = {"pixel_values": x}
         return self.model(**inputs)
-    
+
+
 def from_config(config_path: str):
     """Create a ViT model from configuration
 
@@ -22,5 +26,4 @@ def from_config(config_path: str):
     """
     with open(config_path, "r") as f:
         config = json.load(f)
-        return ViT(patch_size=config['patch_size'])
-        
+        return ViT(patch_size=config["patch_size"], image_size=config["image_size"])
