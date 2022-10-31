d85d405cf402876df26f1c27b56b9b27b8e0e56d
petros <petros_94@icloud.com>
2022-10-31 19:34:31 +0200

    angular loss

--- training/train.py
+++ training/train.py
@@ -1,5 +1,5 @@
 import json
-
+from training.train_cnn_angular import train_angular_loss
 from training.train_cnn_triplet import train_triplet_loss
 
 
@@ -9,6 +9,17 @@ def train(model, train_set, valid_set, config_path, checkpoint_dir, results_dir)
 
     if config["loss"] == "triplet":
         return train_triplet_loss(
+            model,
+            train_set,
+            valid_set,
+            config["train"]["n_epochs"],
+            config["train"]["patience"],
+            config["train"]["lr"],
+            checkpoint_dir,
+            results_dir,
+        )
+    elif config['loss'] == "angular":
+        return train_angular_loss(
             model,
             train_set,
             valid_set,
