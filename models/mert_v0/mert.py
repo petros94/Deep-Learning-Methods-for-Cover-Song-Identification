b6dd66f9e0e08e0a3f7cfde1727c53289331dead
petrosmitseas <petros_94@icloud.com>
2023-01-29 20:33:10 +0200

    Revert "revert asap 3 fml"
    This reverts commit e4541875a48068e5b46a39d699f5a54948576a4c.

--- models/mert_v0/mert.py
+++ models/mert_v0/mert.py
@@ -33,14 +33,16 @@ class MERT(nn.Module):
             hash_key = str(song_ids)
 
         if hash_key is not None and hash_key in self.cache.keys():
-            time_reduced_hidden_states = self.cache[hash_key].to(device)
+            outputs = self.cache[hash_key]
         else:
             with torch.no_grad():
                 outputs = self.model(input_values=x, output_hidden_states=True)
-                all_layer_hidden_states = torch.stack(outputs.hidden_states).squeeze()
-                time_reduced_hidden_states = all_layer_hidden_states.mean(0).squeeze(0)
-            print("added to cache")
-            self.cache[hash_key] = time_reduced_hidden_states.to("cpu")
+
+        if hash_key is not None:
+            self.cache[hash_key] = outputs
+
+        all_layer_hidden_states = torch.stack(outputs.hidden_states).squeeze()
+        time_reduced_hidden_states = all_layer_hidden_states.mean(0).squeeze(0)
 
         output, (h_n, c_n) = self.lstm(time_reduced_hidden_states)
         output = output[:, -1, :]
