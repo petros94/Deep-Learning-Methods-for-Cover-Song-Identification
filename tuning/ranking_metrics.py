374ffa0f9431e352457ffb27926448dc3ad6a081
petrosmitseas <petros_94@icloud.com>
2023-02-12 04:04:45 +0200

    minor feat extr

--- tuning/ranking_metrics.py
+++ tuning/ranking_metrics.py
@@ -20,7 +20,7 @@ def generate_metrics(model, data_set, segmented, results_path):
         distances, clf_labels = generate_posteriors_segments(model, data_set)
         df = generate_ROC(distances, clf_labels, results_path)
         df_prc = generate_PRC(distances, clf_labels, results_path)
-        return df, None, None, None, None
+        return df, 0, 0, 0, 0
     else:
         distances, clf_labels, embeddings, song_labels, cover_names = generate_posteriors_full(model, data_set)
         df = generate_ROC(distances, clf_labels, results_path)
@@ -180,45 +180,31 @@ def calc_MRR(distance_matrix, labels):
     mrr = np.mean(rr)
     return mrr
 
-def calc_MAP(array2d, version, que_range=None, K=1e10):
-    if que_range is not None:
-        que_s, que_t = que_range[0], que_range[1]
-        if que_s == 0:
-            ref_s, ref_t = que_t, len(array2d)
-        else:
-            ref_s, ref_t = 0, que_s
-    else:
-        que_s, que_t, ref_s, ref_t = 0, len(array2d), 0, len(array2d)
-
-    new_array2d = []
-    for u, row in enumerate(array2d[que_s: que_t]):
-        row = [(v + ref_s, col) for (v, col) in enumerate(row[ref_s: ref_t]) if u + que_s != v + ref_s]
-        new_array2d.append(row)
+def calc_MAP(array2d, version):
     MAP, top10, rank1 = 0, 0, 0
 
-    for u, row in enumerate(new_array2d):
-        row.sort(key=lambda x: x[1])
+    for d, lab in zip(array2d, version):
         per_top10, per_rank1, per_MAP = 0, 0, 0
         version_cnt = 0.
-        u = u + que_s
-        for k, (v, val) in enumerate(row):
 
-            if version[u] == version[v]:
+        sorted_ids_by_dist = d.argsort()
+        sorted_labels_by_dist = version[sorted_ids_by_dist][1:]
 
-                if k < K:
-                    version_cnt += 1
-                    per_MAP += version_cnt / (k + 1)
-                if per_rank1 == 0:
-                    per_rank1 = k + 1
-                if k < 10:
-                    per_top10 += 1
+        ids = torch.argwhere(sorted_labels_by_dist == lab)
+        for id in ids:
+            version_cnt += 1
+            per_MAP += version_cnt / (id + 1)
+            if per_rank1 == 0:
+                per_rank1 = id + 1
+            if id < 10:
+                per_top10 += 1
+
         per_MAP /= 1 if version_cnt < 0.0001 else version_cnt
-        # if per_MAP < 0.1:
-        #     print row
         MAP += per_MAP
         top10 += per_top10
         rank1 += per_rank1
-    return MAP / float(que_t - que_s), top10 / float(que_t - que_s) / 10, rank1 / float(que_t - que_s)
+
+    return MAP / float(len(array2d)), top10 / float(len(array2d)) / 10, rank1 / float(len(array2d))
 
 
 def ranking_metrics(model, data_set, k):
