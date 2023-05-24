f9869487289c2128a2b884e0d8746324efb247b4
petrosmitseas <petros_94@icloud.com>
2023-05-24 23:59:19 +0300

    fixed script

--- experiments/realtime_experiment.py
+++ experiments/realtime_experiment.py
@@ -10,53 +10,57 @@ from models.factory import make_model
 import os
 import uuid
 import json
-import torch 
+import torch
 import shutil
 from datetime import datetime
 
 from songbase.load_songs import from_config
 from utils.generic import get_device, segment_and_scale
 
-def download_songs_and_inference(config_path: str, links: list, tmp_base_dir="/content"): 
+
+def download_songs_and_inference(config_path: str, links: list, tmp_base_dir="/content"):
     device = get_device()
     with open(config_path, "r") as f:
         config = json.load(f)
-               
+
     model = make_model(config_path=config_path)
     thr = config['model']['threshold']
     clf = ThresholdClassifier(model, thr)
     clf.eval()
-    
-    scale = (1, 0.2)
+    clf.to(device)
+
+    scale = (1, 0.1)
     frame_size = 4400
     idx = 0
-    
+
     # Download songs to temp dir
     downloader = YoutubeDownloader()
     temp_dir = tmp_base_dir + "/" + str(uuid.uuid4())
     os.makedirs(temp_dir)
     for link in links:
         downloader.download(path=temp_dir, link=link)
-        
+
     # Extract features
-    extractor = FeatureExtractor(features=config['representation'])
+    extractor = FeatureExtractor(features=config['representation'][0])
     entries = os.listdir(temp_dir)
     features = {}
     for song in entries:
         feat = extractor.extract(os.path.realpath(song))
-        
+
         # tensor of size num_segs X num_channels X num_features X num_samples
         features[song] = segment_and_scale(feat, frame_size, scale)
 
     # Forward pass
     for song_1, value_1 in features.items():
         for song_2, value_2 in features.items():
-            x_1 = torch.tensor(value_1[idx]).unsqueeze(0).to(device)
-            x_2 = torch.tensor(value_2[idx]).unsqueeze(0).to(device)
-            
-            pred, dist = clf(x_1, x_2)
-            print(f"Song 1: {song_1}, Song 2: {song_2}, are covers: {pred.item()==True}, distance: {dist.item()}")
-            
+            if song_1 != song_2:
+                x_1 = torch.tensor(value_1[idx]).unsqueeze(0).to(device)
+                x_2 = torch.tensor(value_2[idx]).unsqueeze(0).to(device)
+
+                pred, dist = clf(x_1, x_2)
+                print(
+                    f"- Song 1: {song_1}, Song 2: {song_2} \n are covers: {pred.item() == True}, distance: {dist.item()}")
+        print("")
     # Remove temp dir
     shutil.rmtree(temp_dir)
 
